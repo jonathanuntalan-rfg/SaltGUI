f455485169f25f75d78fdfc70e0276b7d528affd
Erwin Dondorp <saltgui@dondorp.com>
2018-07-18 22:02:51 +0200

    Added support for batch

--- saltgui/static/scripts/CommandBox.js
+++ saltgui/static/scripts/CommandBox.js
@@ -21,7 +21,7 @@ export class CommandBox {
     this.documentation = new Documentation(this);
     this._registerEventListeners();
 
-    RunType.createMenu();
+    RunType.createMenus();
     TargetType.createMenu();
 
     const title = document.querySelector(".run-command #templatemenuhere");
@@ -119,7 +119,15 @@ export class CommandBox {
     output.innerText = "Loading...";
 
     func.then(response => {
-      this._onRunReturn(response.return[0], command);
+      // The data.return array may contain the answer from several minion groups
+      // combine these first into one group for easier processing
+      const allResponses = { };
+      for(const group of response.return) {
+        for(const host in group) {
+          allResponses[host] = group[host];
+        }
+      }
+      this._onRunReturn(allResponses, command);
     });
   }
 
@@ -301,6 +309,12 @@ export class CommandBox {
       params.client = "local_async";
       // return looks like:
       // { "jid": "20180718173942195461", "minions": [ ... ] }
+    }
+    else if(params.client === "local" && runType === "batch") {
+      params.client = "local_batch";
+      params.batch = RunType.getBatchSize();
+      params.batch_wait = RunType.getBatchWait();
+      // it returns the actual output in a list of batches
     }
 
     return this.api.apiRequest("POST", "/", params)
